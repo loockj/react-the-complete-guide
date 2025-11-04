4f3ff15e9a48d1a73d9ff0d57f1ccd603c0a41e4
Jelle Van Loock <22350389+jellevanloock@users.noreply.github.com>
2025-11-04 14:30:15 +0100

    chore: add chapter 10: Context API & useReducer - Advanced State Management

---
+++ chapter-10-context/01-starting-project/src/components/CartModal.jsx
@@ -0,0 +1,30 @@
+import { forwardRef, useImperativeHandle, useRef, useContext } from 'react';
+import { CartContext } from '../store/ShoppingCartContext';
+import { createPortal } from 'react-dom';
+import Cart from './Cart';
+
+const CartModal = forwardRef(function Modal({ title, actions }, ref) {
+    const { items } = useContext(CartContext);
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            open: () => {
+                dialog.current.showModal();
+            },
+        };
+    });
+
+    return createPortal(
+        <dialog id="modal" ref={dialog}>
+            <h2>{title}</h2>
+            <Cart />
+            <form method="dialog" id="modal-actions">
+                {actions}
+            </form>
+        </dialog>,
+        document.getElementById('modal')
+    );
+});
+
+export default CartModal;
