2344300734c1af97dd3b233469ff0568040861f3
Jelle Van Loock <22350389+jellevanloock@users.noreply.github.com>
2025-12-02 13:10:08 +0100

    chore: add chapter 12: Practice project

---
+++ chapter-12-practice-project/01-starting-project/src/components/Answers.jsx
@@ -0,0 +1,33 @@
+import { useRef } from 'react';
+
+export default function Answers({ answers, selectedAnswer, answerState, onSelect }) {
+    const shuffledAnswers = useRef();
+    if (!shuffledAnswers.current) {
+        shuffledAnswers.current = [...answers];
+        shuffledAnswers.current.sort((a, b) => Math.random() - 0.5);
+    }
+    return (
+        <ul id="answers">
+            {shuffledAnswers.current.map((answer, index) => {
+                let cssClass = '';
+                const isSelected = selectedAnswer === answer;
+
+                if (answerState === 'answered' && isSelected) {
+                    cssClass += 'selected';
+                } else if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                    cssClass += answerState;
+                }
+                return (
+                    <li key={index} className="answer">
+                        <button
+                            onClick={() => onSelect(answer)}
+                            className={cssClass}
+                            disabled={answerState !== ''}>
+                            {answer}
+                        </button>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
